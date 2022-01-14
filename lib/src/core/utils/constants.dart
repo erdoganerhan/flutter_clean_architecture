22e2ac938cb7fd4bd9e73499c6c6e8d376821f18
Erhan Erdogan <erhanerdogn@icloud.com>
2022-01-14 23:02:03 +0300

    add const file

---
+++ lib/src/core/utils/constants.dart
@@ -0,0 +1,5 @@
+const String appName = "Clean Architecture";
+const String restApiBaseUrl = "https://newsapi.org/v2";
+const String newsApiKey = "";
+const String articleTableName = "articles_table";
+const String dbName = "app_database.db";
