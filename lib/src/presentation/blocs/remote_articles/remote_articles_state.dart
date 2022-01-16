1ab570742e6a7233e674187aeece56fc9ef08b22
Erhan Erdogan <erhanerdogn@icloud.com>
2022-01-16 18:36:39 +0300

    add package flutter_hooks

--- lib/src/presentation/blocs/remote_articles/remote_articles_state.dart
+++ lib/src/presentation/blocs/remote_articles/remote_articles_state.dart
@@ -1,10 +1,27 @@
 part of 'remote_articles_bloc.dart';
 
 abstract class RemoteArticlesState extends Equatable {
-  const RemoteArticlesState();
-  
+  final List<Article> articles;
+  final bool noMoreData;
+
+  final DioError? error;
+
+  const RemoteArticlesState(
+      {this.articles = const <Article>[], this.noMoreData = false, this.error});
+
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [articles, noMoreData, error];
 }
 
-class RemoteArticlesInitial extends RemoteArticlesState {}
+class RemoteArticlesLoading extends RemoteArticlesState {
+  const RemoteArticlesLoading();
+}
+
+class RemoteArticlesDone extends RemoteArticlesState {
+  const RemoteArticlesDone(List<Article> article, {required bool noMoreData})
+      : super(articles: article, noMoreData: noMoreData);
+}
+
+class RemoteArticlesError extends RemoteArticlesState {
+  const RemoteArticlesError(DioError error) : super(error: error);
+}
