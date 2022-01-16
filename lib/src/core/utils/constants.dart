c84ff498da16459c1b5455473d2cfdd4eda189c4
Erhan Erdogan <erhanerdogn@icloud.com>
2022-01-16 12:25:37 +0300

    integrate apiKey

--- lib/src/core/utils/constants.dart
+++ lib/src/core/utils/constants.dart
@@ -1,5 +1,5 @@
 const String kAppName = "Clean Architecture";
 const String kRestApiBaseUrl = "https://newsapi.org/v2";
-const String kNewsApiKey = "";
+const String kNewsApiKey = "5cf91bfc6d1c45f6931e50d8368785c0";
 const String kArticleTableName = "articles_table";
 const String kDbName = "app_database.db";
