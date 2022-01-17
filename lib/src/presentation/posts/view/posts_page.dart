8c23aedaa2adb8eca02c16282e0fd9bddbd0a0bc
Erhan Erdogan <erhanerdogn@icloud.com>
2022-01-17 15:44:56 +0300

    some changes

--- lib/src/presentation/posts/view/posts_page.dart
+++ lib/src/presentation/posts/view/posts_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_clean_architecture/src/core/utils/constants.dart';
 import '../../../injector.dart';
 import '../posts.dart';
 
@@ -9,7 +10,11 @@ class PostsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Posts')),
+      appBar: AppBar(
+          title: const Text(
+        kAppName,
+        style: TextStyle(color: Colors.black),
+      )),
       body: BlocProvider<PostBloc>(
         create: (_) => injector()..add(PostFetched()),
         child: const PostsList(),
